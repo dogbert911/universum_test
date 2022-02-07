63087cd4a9b769fffe2f31513ada2797b8bb4c8d
dogbert911 <98908967+dogbert911@users.noreply.github.com>
2022-02-07 19:15:53 +0200

    Create .universum.py

---
+++ .universum.py
@@ -0,0 +1,8 @@
+from universum.configuration_support import Configuration, Step
+
+configs = Configuration([
+  Step('Long success step', command=['./test.sh', 'Text', '100', '1', '0' ])
+  Step('Success stderr', command=['./test.sh', 'Error text', '20', '2', '0' ])
+  Step('Error stderr', command=['./test.sh', 'Error text', '20', '2', '1' ])
+  Step('Error stdout', command=['./test.sh', 'Normal text', '20', '1', '2' ])
+])
