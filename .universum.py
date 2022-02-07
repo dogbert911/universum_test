bab5f2a0a43cf0e7086ef896834f3bb4907ded4d
dogbert911 <98908967+dogbert911@users.noreply.github.com>
2022-02-07 19:39:17 +0200

    Update .universum.py

--- .universum.py
+++ .universum.py
@@ -5,4 +5,5 @@ configs = Configuration([
   Step('Success stderr', command=['./test.sh', 'Error text', '20', '2', '0' ]),
   Step('Error stderr', command=['./test.sh', 'Error text', '20', '2', '1' ]),
   Step('Error stdout', command=['./test.sh', 'Normal text', '20', '1', '2' ]),
+  Step('Critical step', command=['./test.sh', 'Normal text', '20', '1', '2' ], critical=True),
 ])
